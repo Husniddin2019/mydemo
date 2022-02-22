f1fd23e0ee38c7737a338f74d7452e4ac47707eb
Husniddin2019 <98811010+Husniddin2019@users.noreply.github.com>
2022-02-22 14:16:53 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:untitled8/home.dart';
+
+import 'details.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: home(),
     );
   }
 }
